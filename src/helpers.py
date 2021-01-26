7d200597202b2f1524a325a2a2b347c49593ec6b
Tuomo Hartonen <tuomo.hartonen@helsinki.fi>
2021-01-26 16:54:18 +0200

    Initial commit. Computation and plotting are working, but there is still a discrepancy with results from Fangjie's script.

---
+++ src/helpers.py
@@ -0,0 +1,117 @@
+from math import log2
+from itertools import product
+
+def getP_j(seqs,j,I,J,k,p):
+    #Function that calculates the k-mer frequency distributions for position j.
+    #input parameters:
+    #seqs = list of lists of all input sequences
+    #j = position index
+    #I = total number of sequences
+    #J = length of sequences
+    #k = length of k-mers
+    P_j = {} #key = k-mer, value = frequency
+    
+    if p>0:
+        #adding pseudocount p to each k-mer count
+        alphabet = ['A','C','G','T']
+        kmers = [''.join(c) for c in product(alphabet,repeat=k)]
+        for kmer in kmers: P_j[kmer] = p
+        
+        for i in range(0,I):
+            kmer = seqs[i][j:j+k]
+            P_j[kmer] += 1.0
+            
+        #normalize counts
+        summ = 0.0
+        for kmer in P_j: summ += P_j[kmer]        
+        for kmer in P_j: P_j[kmer] /= (float(I)+p*pow(4,k))#(float(I)+p*4**k)
+          
+    else: 
+        for i in range(0,I):
+            kmer = seqs[i][j:j+k]
+            if kmer not in P_j: P_j[kmer] = 1.0
+            else: P_j[kmer] += 1.0
+            
+        #normalize counts
+        for kmer in P_j:
+            P_j[kmer] /= float(I)
+      
+    
+    
+    return P_j
+
+def getMI_mn(seqs,m,n,I,J,P_j,k,p):
+    #Function that calculates mutual information between k-mers starting at position j
+    #and all other positions after j
+    #input parameters:
+    #seqs = list of lists of all input sequences
+    #m = position index 1 for the MI pair
+    #n = position index 2 for the MI pair
+    #I = total number of sequences
+    #J = length of sequences
+    #P = singe site k-mer frequencies
+    #k = k-mer length
+    
+    #calculate pairwise frequencies for positions m and n
+    P_mn = {} #key = k-mer pair, for example "AAA" and "ACG is coded as "AAAACG", value = frequency
+    
+    if p>0:
+        #adding pseudocount p to each k-mer count
+        alphabet = ['A','C','G','T']
+        kmers = [''.join(c) for c in product(alphabet,repeat=2*k)]
+        for kmer in kmers: P_mn[kmer] = p#/(float(I)+p)**2
+        #P_m = {}
+        #P_n = {}
+        for i in range(0,I):
+            kmer1 = seqs[i][m:m+k]
+            kmer2 = seqs[i][n:n+k]
+            kmer = kmer1+kmer2
+            P_mn[kmer] += 1.0#/(float(I)+p)**2
+            
+            #if kmer1 not in P_m: P_m[kmer1] = 1.0
+            #else: P_m[kmer1] += 1.0
+            
+            #if kmer2 not in P_n: P_n[kmer2] = 1.0
+            #else: P_n[kmer2] += 1.0
+            
+        
+        #normalize counts
+        summ = 0.0
+        for kmer in P_mn:
+            #kmer1 = kmer[:k]
+            #kmer2 = kmer[k:]
+            #P_mn[kmer] += (P_mn[kmer]*(p/P_m[kmer1]+p/P_n[kmer2]))
+            summ += P_mn[kmer]
+        
+        #print("summ="+str(summ)+" / "+str((float(I)+p*pow(4,2*k))))
+        for kmer in P_mn: P_mn[kmer] /= (float(I)+p*pow(4,2*k))#(float(I)+p*4**(2*k))#summ
+        
+    else:
+        for i in range(0,I):
+            kmer1 = seqs[i][m:m+k]
+            kmer2 = seqs[i][n:n+k]
+            if kmer1+kmer2 not in P_mn: P_mn[kmer1+kmer2] = 1.0
+            else: P_mn[kmer1+kmer2] += 1.0
+              
+        #normalize counts
+        summ = 0
+        for kmer in P_mn:
+            P_mn[kmer] /= float(I)
+    #if m==0:
+    #    print("m="+str(m)+", n="+str(n))
+    #    print(P_mn)
+    #calculate mutual information
+    #MI(m,n) = \sum_{kmer_m \in K} \sum_{kmer_n in K} P_mn(kmer_m,kmer_n)*log_2(P_mn(kmer_m,kmer_n)/(P_m(kmer_m)*P_n(kmer_n)))
+    #where K is the set of all observed k-mers at positions m and n
+    MI_mn = 0.0
+    for kmer_mn in P_mn:
+        kmer_m = kmer_mn[:k]
+        kmer_n = kmer_mn[k:]
+        #if m==0 and n==3: print(kmer_mn+": "+kmer_m+", "+kmer_n+"| P_mn="+str(P_mn[kmer_mn]))
+        #if kmer_mn not in P_mn: continue
+        #if m==0 and n==3: print(P_mn[kmer_mn]*log2(P_mn[kmer_mn]/(P_j[m][kmer_m]*P_j[n][kmer_n])))
+        MI_mn += P_mn[kmer_mn]*log2(P_mn[kmer_mn]/(P_j[m][kmer_m]*P_j[n][kmer_n]))
+            
+    return [(m,n),MI_mn]
+    
+    
---
+++ src/plotMI.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+import matplotlib
+matplotlib.use("Agg")
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+from time import time
+import argparse
+import csv
+import numpy as np
+
+import multiprocessing as mp
+
+from helpers import getP_j, getMI_mn
+
+def plotMI():
+
+    ########################
+    #command line arguments#
+    ########################
+
+    parser = argparse.ArgumentParser()
+
+    #PARAMETERS
+    parser.add_argument("--outdir",help="Full path to the output directory.",type=str)
+    parser.add_argument("--seqs",help="Full path to the plain text input sequence file. Each sequence must be of same length and on its separate line.",type=str)
+    parser.add_argument("--nproc",help="Number of parallel processes used when computing MI (default=1).",type=int,default=1)
+    parser.add_argument("--figtype",help="png or pdf (default=png).",type=str,choices=['pdf','png'],default='png')
+    parser.add_argument("--k",help="length of k-mer distributions used to calculate MI (default=3).",type=int,default=3)
+    parser.add_argument("--v",help="Verbosity level, 0=none, 1=print info on screen (default=1).",type=int,choices=[0,1],default=1)
+    parser.add_argument("--p",help="Pseudocount added to each k-mer count (default=0).",type=int,default=0)
+    args = parser.parse_args()
+    
+    #read in the sequences and store them as strings
+    start = time()
+    seqs = []
+    with open(args.seqs,'rt') as infile:
+        r = csv.reader(infile)
+        for row in r:
+            if len(row)<1: continue
+            seqs.append(row[0])
+    
+    I = len(seqs) #number of sequences
+    J = len(seqs[0]) #length of sequences
+    
+    end = time()
+    if args.v>0: print("Read in "+str(I)+" sequences of length "+str(J)+" in "+str(end-start)+" seconds.")
+    
+    start = time()
+    #calculate single site frequencies of k-mers in parallel
+    pool = mp.Pool(args.nproc)
+    res = [pool.apply_async(getP_j,args=(seqs,j,I,J,args.k,args.p)) for j in range(0,J-args.k)]
+    P = [r.get() for r in res] #list containing the single site k-mer frequencies. Each distribution is saved as a dictionary where k-mer is key and its frequency is value
+    
+    #print(P[0])
+    #print(sum([P[0][kmer] for kmer in P[0]]))
+    pool.close()
+    pool.join()
+    pool.terminate()
+    
+    end = time()
+    if args.v>0: print("Calculated the positional "+str(args.k)+"-mer frequencies in "+str(end-start)+" seconds.")
+    
+    start = time()
+    #calculate MI in parallel
+    pool = mp.Pool(args.nproc)
+    res = []
+    
+    M = 0 #number of pairwise k-mer distributions
+    for m in range(0,J-2*args.k):
+        M += 1
+        #overlapping_inds = set([l for l in range(m-args.k,m+args.k)])
+        for n in range(m+args.k,J-args.k):
+            #if n in overlapping_inds: continue
+            #print("m="+str(m)+", n="+str(n))
+            res.append(pool.apply_async(getMI_mn,args=(seqs,m,n,I,J,P,args.k,args.p)))
+    res = [r.get() for r in res]    
+
+    pool.close()
+    pool.join()
+    pool.terminate()
+    
+    #build the MI matrix
+    #print("M="+str(M))
+    MI = np.zeros(shape=(M,M))
+    #print(MI.shape)
+    for j in range(0,len(res)):
+        inds = res[j][0]
+        #print(inds)
+        mi = res[j][1]
+        MI[inds[1]-args.k,inds[0]] = mi
+
+    xticks = [res[0][0][1],res[-1][0][1]]
+    yticks = [res[0][0][0],res[-1][0][0]]
+    end = time()
+    if args.v>0: print("Computed mutual information in "+str(end-start)+" seconds.")
+    
+    start = time()
+    #save the MI matrix to file
+    np.savetxt(args.outdir+"MI.txt.gz",MI,delimiter='\t')
+    
+    #plot the lower triangle of the MI-matrix
+    mask = np.zeros_like(MI)
+    mask[np.triu_indices_from(mask)] = True
+    print(MI)
+    sns_plot = sns.heatmap(MI,cmap='Blues',cbar=True,cbar_kws={'label': 'MI'},mask=mask,xticklabels=xticks,yticklabels=yticks)
+    sns_plot.set_xticks([i-args.k for i in xticks])
+    sns_plot.set_yticks(yticks)
+    fig = sns_plot.get_figure()
+    #fig.set_xlabel('position')
+    #fig.set_ylabel('position')
+    fig.savefig(args.outdir+"MI."+args.figtype,dpi=150)
+    
+    end = time()
+    if args.v>0: print("Plotting done in "+str(end-start)+" seconds.")
+#end
+
+plotMI()
