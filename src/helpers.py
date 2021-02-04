5c566e414215cf10ba1d7d30b4a886a96c699e07
Hartonen Tuomo S <thartone@pangolin-18.it.helsinki.fi>
2021-02-04 11:35:58 +0200

    Added possibility to use any alphabet. Changed heatmap colormap to jet.

--- src/helpers.py
+++ src/helpers.py
@@ -1,7 +1,7 @@
 from math import log2
 from itertools import product
 
-def getP_j(seqs,j,I,J,k,p):
+def getP_j(seqs,j,I,J,k,p,alphabet):
     #Function that calculates the k-mer frequency distributions for position j.
     #input parameters:
     #seqs = list of lists of all input sequences
@@ -10,13 +10,14 @@ def getP_j(seqs,j,I,J,k,p):
     #J = length of sequences
     #k = length of k-mers
     #p = pseudocount mass
+    #alphabet = alphabet used
+    n_a = len(alphabet)
     P_j = {} #key = k-mer, value = frequency
     
     if p>0:
         #adding pseudocount p to each k-mer count
-        alphabet = ['A','C','G','T']
         kmers = [''.join(c) for c in product(alphabet,repeat=k)]
-        for kmer in kmers: P_j[kmer] = p/pow(4,k)#p
+        for kmer in kmers: P_j[kmer] = p/pow(n_a,k)#p
         
         for i in range(0,I):
             kmer = seqs[i][j:j+k]
@@ -41,7 +42,7 @@ def getP_j(seqs,j,I,J,k,p):
     
     return P_j
 
-def getMI_mn(seqs,m,n,I,J,P_j,k,p):
+def getMI_mn(seqs,m,n,I,J,P_j,k,p,alphabet):
     #Function that calculates mutual information between k-mers starting at position j
     #and all other positions after j
     #input parameters:
@@ -53,15 +54,15 @@ def getMI_mn(seqs,m,n,I,J,P_j,k,p):
     #P = singe site k-mer frequencies
     #k = k-mer length
     #p = pseudocount mass
-    
+    #alphabet = alphabet used
+    n_a = len(alphabet)
     #calculate pairwise frequencies for positions m and n
     P_mn = {} #key = k-mer pair, for example "AAA" and "ACG is coded as "AAAACG", value = frequency
     
     if p>0:
-        #adding pseudocount p to each k-mer count
-        alphabet = ['A','C','G','T']
+        #adding pseudocount p/n_a^2k to each k-mer count
         kmers = [''.join(c) for c in product(alphabet,repeat=2*k)]
-        for kmer in kmers: P_mn[kmer] = p/pow(4,2*k)#p
+        for kmer in kmers: P_mn[kmer] = p/pow(n_a,2*k)#p
         #P_m = {}
         #P_n = {}
         CAA_count = 0
@@ -71,13 +72,6 @@ def getMI_mn(seqs,m,n,I,J,P_j,k,p):
             if kmer2=='CAA': CAA_count += 1
             kmer = kmer1+kmer2
             P_mn[kmer] += 1.0#/(float(I)+p)**2
-            
-            #if kmer1 not in P_m: P_m[kmer1] = 1.0
-            #else: P_m[kmer1] += 1.0
-            
-            #if kmer2 not in P_n: P_n[kmer2] = 1.0
-            #else: P_n[kmer2] += 1.0
-            
         
         #normalize counts
         summ = 0.0
@@ -102,9 +96,7 @@ def getMI_mn(seqs,m,n,I,J,P_j,k,p):
         summ = 0
         for kmer in P_mn:
             P_mn[kmer] /= float(I)
-    #if m==0:
-    #    print("m="+str(m)+", n="+str(n))
-    #    print(P_mn)
+    
     #calculate mutual information
     #MI(m,n) = \sum_{kmer_m \in K} \sum_{kmer_n in K} P_mn(kmer_m,kmer_n)*log_2(P_mn(kmer_m,kmer_n)/(P_m(kmer_m)*P_n(kmer_n)))
     #where K is the set of all observed k-mers at positions m and n
@@ -121,36 +113,28 @@ def getMI_mn(seqs,m,n,I,J,P_j,k,p):
                 if kmer_m=='GTG': MI_GTG_sum += MI_auxs[-1]
                 else: MI_others_sum += MI_auxs[-1]
             MI_mn += P_mn[kmer_mn]*log2(P_mn[kmer_mn]/(P_j[m][kmer_m]*P_j[n][kmer_n]))
-            if m==5 and n==80 and kmer_m=='GTG' and kmer_n=='CAA' and p>0: print("MI_5,80^(GTG,CAA)="+str(P_mn[kmer_mn]*log2(P_mn[kmer_mn]/(P_j[m][kmer_m]*P_j[n][kmer_n]))))
-            if m==5 and n==80 and kmer_m=='CAA' and kmer_n=='CAA' and p>0: print("MI_5,80^(CAA,CAA)="+str(P_mn[kmer_mn]*log2(P_mn[kmer_mn]/(P_j[m][kmer_m]*P_j[n][kmer_n]))))
-            if m==60 and n==80 and kmer_m=='GTG' and kmer_n=='CAA' and p>0: print("MI_60,80^(GTG,CAA)="+str(P_mn[kmer_mn]*log2(P_mn[kmer_mn]/(P_j[m][kmer_m]*P_j[n][kmer_n]))))
+            #if m==5 and n==80 and kmer_m=='GTG' and kmer_n=='CAA' and p>0: print("MI_5,80^(GTG,CAA)="+str(P_mn[kmer_mn]*log2(P_mn[kmer_mn]/(P_j[m][kmer_m]*P_j[n][kmer_n]))))
+            #if m==5 and n==80 and kmer_m=='CAA' and kmer_n=='CAA' and p>0: print("MI_5,80^(CAA,CAA)="+str(P_mn[kmer_mn]*log2(P_mn[kmer_mn]/(P_j[m][kmer_m]*P_j[n][kmer_n]))))
+            #if m==60 and n==80 and kmer_m=='GTG' and kmer_n=='CAA' and p>0: print("MI_60,80^(GTG,CAA)="+str(P_mn[kmer_mn]*log2(P_mn[kmer_mn]/(P_j[m][kmer_m]*P_j[n][kmer_n]))))
     
-    if m==5 and n==80 and p>0:
-            print("P_5,80(GTG,CAA)="+str(P_mn["GTGCAA"]))
-            print("P_5(GTG)="+str(P_j[m]["GTG"]))
-            print("P_80(CAA)="+str(P_j[n]["CAA"]))
-            print("observed number of pairs = "+str(summ))
-            print("CAA count="+str(CAA_count))
-            print("MI_5,80="+str(MI_mn))
-            print("MI_GTG_sum="+str(MI_GTG_sum))
-            print("MI_others_sum"+str(MI_others_sum))
-            #print(MI_auxs)
-    if m==60 and n==80 and p>0:
-            print("P_60,80(GTG,CAA)="+str(P_mn["GTGCAA"]))
-            print("P_60(GTG)="+str(P_j[m]["GTG"]))
-            print("P_80(CAA)="+str(P_j[n]["CAA"]))
-            print("observed number of pairs = "+str(summ))
-            print("CAA count="+str(CAA_count))
-            print("MI_60,80="+str(MI_mn))
-            print("sum(MI_auxs)="+str(sum(MI_auxs)))
-    
-    #for kmer_mn in P_mn:
-    #    kmer_m = kmer_mn[:k]
-    #    kmer_n = kmer_mn[k:]
-    #    #if m==0 and n==3: print(kmer_mn+": "+kmer_m+", "+kmer_n+"| P_mn="+str(P_mn[kmer_mn]))
-    #    #if kmer_mn not in P_mn: continue
-    #    #if m==0 and n==3: print(P_mn[kmer_mn]*log2(P_mn[kmer_mn]/(P_j[m][kmer_m]*P_j[n][kmer_n])))
-    #    MI_mn += P_mn[kmer_mn]*log2(P_mn[kmer_mn]/(P_j[m][kmer_m]*P_j[n][kmer_n]))
+    #if m==5 and n==80 and p>0:
+    #        print("P_5,80(GTG,CAA)="+str(P_mn["GTGCAA"]))
+    #        print("P_5(GTG)="+str(P_j[m]["GTG"]))
+    #        print("P_80(CAA)="+str(P_j[n]["CAA"]))
+    #        print("observed number of pairs = "+str(summ))
+    #        print("CAA count="+str(CAA_count))
+    #        print("MI_5,80="+str(MI_mn))
+    #        print("MI_GTG_sum="+str(MI_GTG_sum))
+    #        print("MI_others_sum"+str(MI_others_sum))
+    #        #print(MI_auxs)
+    #if m==60 and n==80 and p>0:
+    #        print("P_60,80(GTG,CAA)="+str(P_mn["GTGCAA"]))
+    #        print("P_60(GTG)="+str(P_j[m]["GTG"]))
+    #        print("P_80(CAA)="+str(P_j[n]["CAA"]))
+    #        print("observed number of pairs = "+str(summ))
+    #        print("CAA count="+str(CAA_count))
+    #        print("MI_60,80="+str(MI_mn))
+    #        print("sum(MI_auxs)="+str(sum(MI_auxs)))
             
     return [(m,n),MI_mn]
     
--- src/plotMI.py
+++ src/plotMI.py
@@ -31,6 +31,7 @@ def plotMI():
     parser.add_argument("--k",help="length of k-mer distributions used to calculate MI (default=3).",type=int,default=3)
     parser.add_argument("--v",help="Verbosity level, 0=none, 1=print info on screen (default=1).",type=int,choices=[0,1],default=1)
     parser.add_argument("--p",help="Multiplier for pseudocount mass added to k-mer count. Total pseudocount mass added is p*(number of sequences) (default=5).",type=int,default=5)
+    parser.add_argument("--alphabet",help="A string containing each individual letter in the alphabet used (default=ACGT). NOTE! This is case-sensitive.",type=str,default="ACGT")
     args = parser.parse_args()
     
     #read in the sequences and store them as strings
@@ -44,7 +45,9 @@ def plotMI():
     
     I = len(seqs) #number of sequences
     J = len(seqs[0]) #length of sequences
-    p = args.p*J #pseudocount mass added to k-mers
+    p = args.p*I #pseudocount mass added to k-mers
+    alphabet = ''.join(set(args.alphabet)) #alphabet used
+    if args.v>0: print("Alphabet: "+alphabet)
     
     end = time()
     if args.v>0: print("Read in "+str(I)+" sequences of length "+str(J)+" in "+str(end-start)+" seconds.")
@@ -52,7 +55,7 @@ def plotMI():
     start = time()
     #calculate single site frequencies of k-mers in parallel
     pool = mp.Pool(args.nproc)
-    res = [pool.apply_async(getP_j,args=(seqs,j,I,J,args.k,p)) for j in range(0,J-args.k+1)]
+    res = [pool.apply_async(getP_j,args=(seqs,j,I,J,args.k,p,alphabet)) for j in range(0,J-args.k+1)]
     P = [r.get() for r in res] #list containing the single site k-mer frequencies. Each distribution is saved as a dictionary where k-mer is key and its frequency is value
     
     pool.close()
@@ -70,7 +73,7 @@ def plotMI():
     M = 0 #number of pairwise k-mer distributions
     for m in range(0,J-2*args.k+1):
         M += 1
-        for n in range(m+args.k,J-args.k+1): res.append(pool.apply_async(getMI_mn,args=(seqs,m,n,I,J,P,args.k,p)))
+        for n in range(m+args.k,J-args.k+1): res.append(pool.apply_async(getMI_mn,args=(seqs,m,n,I,J,P,args.k,p,alphabet)))
     res = [r.get() for r in res]    
 
     pool.close()
@@ -78,7 +81,7 @@ def plotMI():
     pool.terminate()
     
     #build the MI matrix
-    MI = np.zeros(shape=(M,M))
+    MI = -1*np.ones(shape=(M,M))
     for j in range(0,len(res)):
         inds = res[j][0]
         mi = res[j][1]
@@ -95,12 +98,12 @@ def plotMI():
     
     #plot the lower triangle of the MI-matrix
     mask = np.zeros_like(MI)
-    mask[np.triu_indices_from(mask)] = True
-    print(MI)
-    print(ma.masked_array(MI,mask=mask))
-    sns_plot = sns.heatmap(MI,cmap='Blues',cbar=True,cbar_kws={'label': 'MI'},xticklabels=xticks,yticklabels=yticks)
+    mask[MI<0] = True
+
+    sns_plot = sns.heatmap(MI,cmap='jet',cbar=True,cbar_kws={'label': 'MI'},xticklabels=xticks,yticklabels=yticks,mask=mask,vmin=0)
     sns_plot.set_xticks([i-args.k for i in xticks])
     sns_plot.set_yticks(yticks)
+    sns.despine(offset=10, trim=True)
     fig = sns_plot.get_figure()
     #fig.set_xlabel('position')
     #fig.set_ylabel('position')
